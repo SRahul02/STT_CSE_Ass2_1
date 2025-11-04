d918cef37f5052b20032cd573a348ddd77c197e4
SRahul02 <rahulsingal2005@gmail.com>
2025-11-05 00:29:57 +0530

    Implemented task 1 and 2

---
+++ Calc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    internal class Calc
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public string CheckOdd(double num)
+        {
+            if (num%2==1) { return "Odd";}
+            else if (num%2==0) { return "Even"; }
+            else { return "Not an Integer"; }
+        }
+    }
+}
---
+++ Program_Task2.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc call_to_calculator = new Calc();
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double tot = call_to_calculator.Add(num1, num2);
+            double diff = call_to_calculator.Subtract(num1, num2);
+            double prod = call_to_calculator.Multiply(num1, num2);
+            double quot = call_to_calculator.Divide(num1, num2);
+
+            string even_Or_odd = call_to_calculator.CheckOdd(tot);
+            Console.WriteLine($"\n--- Results ---");
+            Console.WriteLine($"Addition: {num1} + {num2} = {tot}");
+            Console.WriteLine($"Subtraction: {num1} - {num2} = {diff}");
+            Console.WriteLine($"Multiplication: {num1} * {num2} = {prod}");
+            Console.WriteLine($"Division: {num1} / {num2} = {quot}");
+            Console.WriteLine($"The sum ({tot}) is: {even_Or_odd}");
+        }
+    }
+}
